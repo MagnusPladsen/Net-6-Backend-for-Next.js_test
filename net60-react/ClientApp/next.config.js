fdc3a2e5eda98cb519951a8df2d343e89d171884
Magnus Pladsen <93226629+MagnusPladsen@users.noreply.github.com>
2022-12-02 10:56:05 +0100

    Started config for nextjs to work with .Net6

---
+++ net60-react/ClientApp/next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+
+const rewrites = () => {
+  return [
+    {
+      source: "/api/:path*",
+      destination: "http://localhost:5204/api/:path*",
+    },
+  ];
+};
+
+const nextConfig = {
+  reactStrictMode: true,
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+  images: {
+    loader: "akamai",
+    path: "/",
+  },
+  rewrites,
+};
+
+module.exports = nextConfig;
---
+++ net60-react/ClientApp/pages/products/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ProductIndex() {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
